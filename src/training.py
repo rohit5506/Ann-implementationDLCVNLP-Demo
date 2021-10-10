c31eb2a894bd2a628f33a884669c8726f5a96b7b
Rohit Bisht <bishtrohit097@gmail.com>
2021-10-10 18:01:29 +0530

    added a save model function.

--- src/training.py
+++ src/training.py
@@ -1,12 +1,14 @@
 from utils.common import read_config
 from utils.data_mgmt import get_data
-from utils.model import create_model
+from utils.model import create_model, save_model
+import os
 
 import argparse
 
+
 def training(config_path):
     config = read_config(config_path)
-    
+
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
 
@@ -21,7 +23,18 @@ def training(config_path):
     VALIDATION_SET = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS,
-                    validation_data=VALIDATION_SET)
+                        validation_data=VALIDATION_SET)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,19 +1,30 @@
 import tensorflow as tf
+import time
+import os
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"_%Y%m%dT%H%M%S_{filename}")
+    return unique_filename
+
+
+def save_model(model, model_name,model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model)
 
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
-
     LAYERS = [tf.keras.layers.Flatten(input_shape=[28, 28], name="inputLayer"),
-          tf.keras.layers.Dense(300, activation="relu", name="hiddenLayer1"),
-          tf.keras.layers.Dense(100, activation="relu", name="hiddenLayer2"),
-          tf.keras.layers.Dense(NUM_CLASSES, activation="softmax", name="outputLayer")]
+              tf.keras.layers.Dense(300, activation="relu", name="hiddenLayer1"),
+              tf.keras.layers.Dense(100, activation="relu", name="hiddenLayer2"),
+              tf.keras.layers.Dense(NUM_CLASSES, activation="softmax", name="outputLayer")]
 
     model_clf = tf.keras.models.Sequential(LAYERS)
 
     model_clf.summary()
 
     model_clf.compile(loss=LOSS_FUNCTION,
-                optimizer=OPTIMIZER,
-                metrics=METRICS)
+                      optimizer=OPTIMIZER,
+                      metrics=METRICS)
 
-    return model_clf ## <<< untrained model
+    return model_clf  ## <<< untrained model
